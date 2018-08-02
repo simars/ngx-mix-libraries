228c98501a51b6d0e523c165b63ef9a32704b44a
Simar Singh <simarpaulsingh@gmail.com>
2018-08-01 22:47:12 -0400

    adding npm scripts to build and publish libraries

--- projects/ngx-mix/src/lib/pipe/apply/apply.pipe.ts
+++ projects/ngx-mix/src/lib/pipe/apply/apply.pipe.ts
@@ -8,9 +8,9 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ApplyPurePipe implements PipeTransform {
 
   /**
-   * Transforms the input value with a given function
-   * @param value to map
-   * @param fn , the mapping function
+   * Transforms the input that changes only by <strong>reference</strong> value by applying given <strong>pure</strong>
+   * @param value to to be applied
+   * @param fn , the function to apply.
    */
   transform(value: any, fn: Function): any {
     return fn(value);
@@ -25,9 +25,9 @@ export class ApplyPurePipe implements PipeTransform {
 export class ApplyPipe implements PipeTransform {
 
   /**
-   * Transforms the input value with a given <strong>pure</strong> function
+   * Transforms the input value by applying given function
    * @param value to map
-   * @param fn , the mapping function (function should be pure)
+   * @param fn , the function to apply (function should be pure).
    */
   transform(value: any, fn: Function): any {
     return fn(value);
